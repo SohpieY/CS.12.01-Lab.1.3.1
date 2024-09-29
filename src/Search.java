d9c25670fc8f6f3d3f4b5b983e642d491eeb0a4d
tianlinsy <tianlinsy2026@student.cis.edu.hk>
2024-09-29 15:02:00 +0800

    yay finished

--- src/Search.java
+++ src/Search.java
@@ -3,12 +3,21 @@ import java.util.ArrayList;
 public class Search {
 
     public static int linearSearch(int[] array, int value) {
+        for(int i = 0; i < array.length; i++){
+            if(array[i] == value){
+            return i;
+        }
+        }
 
         return -1;
-
     }
 
     public static int linearSearch(ArrayList<String> list, String string) {
+        for(int i = 0; i < list.size(); i++){
+            if(list.get(i).equals(string)){
+                return i;
+            }
+        }
 
         return -1;
 
@@ -16,13 +25,45 @@ public class Search {
 
     public static int binarySearch(int[] array, int value) {
 
-        return -1;
+        int start = 0;
+        int end = array.length - 1;
 
+        while(start <= end) {
+            int mid = (end + start)/2;
+            if(array[mid] == value){
+                return mid;
+            }
+
+            if(array[mid] < value){ //if value is greater than mid, then ignore left half
+                start = mid + 1;
+            } else{
+                end = mid - 1; //if value is smaller than mid, ignore right half
+            }
+        }
+        return -1;
     }
 
     public static int binarySearch(String[] array, String string) {
+        int start = 0;
+        int end = array.length - 1;
 
+        while(start <= end) {
+
+            int mid = (end + start)/2;
+            int check = string.compareTo(array[mid]);
+
+            if(check == 0){ // Check if value is present at mid
+                return mid;
+            }
+
+            if(check > 0){ //if value is greater than mid, then ignore left half
+                start = mid + 1;
+            } else{
+                end = mid - 1; //if value is smaller than mid, ignore right half
+            }
+        }
         return -1;
+
 
     }
 }
